c360da191fbae2b9b0e61dbd914032b86f706092
jxgacsraccp <jxgacsraccp@163.com>
2014-11-04 17:19:57 +0800

    测试类修改
    添加Test.test3();

--- src/Test.java
+++ src/Test.java
@@ -232,6 +232,7 @@ public class Test
     public static void main(String[] args) throws Exception {
 //        Test.test1();
 //        Test.test2();
+//        Test.test3();
         Test.test3();
     }
 
